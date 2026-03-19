89d1ba2d59b84ad4235a78e34c8bcff8a684f4cd
Ersek Marton <ersekmarton@gmail.com>
2026-03-19 13:51:50 +0100

    example-page-remove

--- src/resources/views/home.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- add the lang.blade.php into your view or layout --}}
-@include('lang')
-
-{{-- declare all your strings with proper translate syntrax --}}
-<h2>{{ __('this is a sentence') }}</h2>
-<h2>{{ __('this is also a sentence') }}</h2>
